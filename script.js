ac7fbe2b2f1c04433ec2327fe293aa448ff649c5
Anneliese Sparks <sparksanneliese@gmail.com>
2021-04-20 15:23:42 -0400

    added timer variable to js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let start = document.getElementById("start");
-console.log(start);
-
 let reset = document.getElementById("reset");
-console.log(reset);
+let timer = document.getElementById("counter");
 
 reset.addEventListener('click', event => {
     document.location.reload(event);
 });
+
