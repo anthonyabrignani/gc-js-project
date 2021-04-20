c4f94eeff4e5fe8eb51b5e218c742ccfcb6b7e57
anthony abrignani <anthony abrignani>
2021-04-20 16:23:47 -0400

    made a timer

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let start = document.getElementById("start");
 let reset = document.getElementById("reset");
 let timer = document.getElementById("counter");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+
+start.addEventListener('click', pad => {
+    var sec = 0;
+    function pad(val) {return val>9 ? val : "0" + val;}
+    setInterval( function(){
+        document.getElementById("seconds").innerHTML=pad(++sec%60);
+        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+    }, 1000);
+});
 
 reset.addEventListener('click', event => {
     document.location.reload(event);
