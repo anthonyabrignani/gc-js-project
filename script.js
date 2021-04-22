f9a24a46045d41f0b3feb98ee23e624650e84c04
Anneliese Sparks <sparksanneliese@gmail.com>
2021-04-22 09:59:06 -0400

    anneliese added card matching and board locker

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ let seconds = document.getElementById("seconds");
 let cards = document.querySelectorAll(".cards");
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 console.log(cards);
 
+// function to shuffle each card randomly
 (function  shuffleCards() {
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 20);
@@ -18,7 +20,9 @@ console.log(cards);
 
 console.log(cards);
 
+// start button to initiate the game
 start.addEventListener('click', pad => {
+    // timer
     var sec = 0;
     function pad(val) {return val>9 ? val : "0" + val;}
     setInterval( function(){
@@ -27,18 +31,59 @@ start.addEventListener('click', pad => {
     }, 1000);
 });
 
+// function to flip cards over
 function flipCards () {
-    //find "cards" 
-    //adds class flip to what we are clicking on so "this" ="cards"
+    // if board is unlocked, run this function to flip cards
+    if (lockBoard) return;
+    // adds class flip to what we are clicking on so "this" ="cards"
     this.classList.add("flip");
-    if (!hasFlipped) {
+    // selects the first card to be flipped
+    if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard =this;
+        return;
     }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkforMatch();
 }
 
+// function to see if cards are a match
+function checkforMatch () {
+    if (firstCard.dataset.match === secondCard.dataset.match) {
+        disableCards();
+        return;
+    }
+
+// if cards are not a match, unflip cards
+    unflipCards();
+}
+
+// function to remove event listener if a match
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+// if not a match, remove the class flip to flip cards back over
+function unflipCards() {
+// when board is locked, flip the cards back over when not a match
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+// after cards are flipped over, the board is set to unlocked
+        lockBoard = false;
+// 2000 refers to milliseconds between flip (2 seconds)
+    }, 2000);
+}
+
+// event listener for when clicked, card will flip
 cards.forEach(card => card.addEventListener("click", flipCards));
 
+// when reset button is clicked, refresh page
 reset.addEventListener('click', event => {
     document.location.reload(event);
 });
